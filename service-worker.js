bd4cbf69228e87286fd50d749b24893423eb85c9
miawizz <miaernst@gmail.com>
2026-02-12 07:21:48 -0600

    Add service worker for asset caching
    Implement service worker for caching core assets and handling fetch requests.

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = "famprov-v1";
+
+const CORE_ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png",
+  "./icons/famprov-logo-horizontal.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((k) => k !== CACHE_NAME)
+          .map((k) => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(req)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(req, copy);
+          });
+          return res;
+        })
+        .catch(() => caches.match("./"));
+    })
+  );
+});
